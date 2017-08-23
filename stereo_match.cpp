22f9310d726cb4ba8ba91909d91f009080fa16cd
zhuying <zhuying@auteltech.net>
2017-08-23 17:14:48 +0800

    can work now.

--- stereo_match.cpp
+++ stereo_match.cpp
@@ -56,56 +56,16 @@ int main(int argc, char** argv)
     int SADWindowSize, numberOfDisparities;
     bool no_display;
     float scale;
-    //capture image from stereo cam
-    VideoCapture capture;
-    Mat frame, left_img, right_img;
-    if(!capture.open(0)){
-        std::cout << "Capture from camera 0 didn't work" << std::endl;
-        return -1;
-    }
-    if( capture.isOpened() )
-    {
-        std::cout << "Video capturing has been started ..." << std::endl;
-//        capture.set(CV_CAP_PROP_FRAME_WIDTH,2560);    //  1280x720
-//        capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
-        capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);  //640x480
-        capture.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-        //capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('M','J','P','G'));
-        capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('Y','U','Y','V'));
-        //capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('Y','V','1','2'));
-        for(;;)
-        {
-            capture >> frame;
-            left_img = frame(Rect(0,0, frame.cols / 2, frame.rows));
-            right_img = frame(Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows));
-            if( frame.empty() )
-                break;
-
-//            Mat frame1 = frame.clone();
-            imshow("camera", frame);
-            imshow("left image", left_img);
-            imshow("right image", right_img);
-
-            char c = (char)waitKey(10);
-            if( c == 27 || c == 'q' || c == 'Q' )
-                break;
-        }
-    }
-
-    return 0;
-
 
     Ptr<StereoBM> bm = StereoBM::create(16,9);
     Ptr<StereoSGBM> sgbm = StereoSGBM::create(0,16,3);
     cv::CommandLineParser parser(argc, argv,
-        "{@arg1||}{@arg2||}{help h||}{algorithm||}{max-disparity|0|}{blocksize|0|}{no-display||}{scale|1|}{i||}{e||}{o||}{p||}");
+        "{help h||}{algorithm||}{max-disparity|0|}{blocksize|0|}{no-display||}{scale|1|}{i||}{e||}{o||}{p||}");
     if(parser.has("help"))
     {
         print_help();
         return 0;
     }
-    img1_filename = parser.get<std::string>(0);
-    img2_filename = parser.get<std::string>(1);
     if (parser.has("algorithm"))
     {
         std::string _alg = parser.get<std::string>("algorithm");
@@ -154,11 +114,6 @@ int main(int argc, char** argv)
         printf("Command-line parameter error: The block size (--blocksize=<...>) must be a positive odd number\n");
         return -1;
     }
-    if( img1_filename.empty() || img2_filename.empty() )
-    {
-        printf("Command-line parameter error: both left and right images must be specified\n");
-        return -1;
-    }
     if( (!intrinsic_filename.empty()) ^ (!extrinsic_filename.empty()) )
     {
         printf("Command-line parameter error: either both intrinsic and extrinsic parameters must be specified, or none of them (when the stereo pair is already rectified)\n");
@@ -172,158 +127,203 @@ int main(int argc, char** argv)
     }
 
     int color_mode = alg == STEREO_BM ? 0 : -1;
-    Mat img1 = imread(img1_filename, color_mode);
-    Mat img2 = imread(img2_filename, color_mode);
 
-    if (img1.empty())
-    {
-        printf("Command-line parameter error: could not load the first input image file\n");
+
+
+    //capture image from stereo cam
+    VideoCapture capture;
+    Mat frame, left_img, right_img;
+    if(!capture.open(0)){
+        std::cout << "Capture from camera 0 didn't work" << std::endl;
         return -1;
     }
-    if (img2.empty())
+    if( capture.isOpened() )
     {
-        printf("Command-line parameter error: could not load the second input image file\n");
-        return -1;
-    }
-
-    if (scale != 1.f)
-    {
-        Mat temp1, temp2;
-        int method = scale < 1 ? INTER_AREA : INTER_CUBIC;
-        resize(img1, temp1, Size(), scale, scale, method);
-        img1 = temp1;
-        resize(img2, temp2, Size(), scale, scale, method);
-        img2 = temp2;
-    }
-
-    Size img_size = img1.size();
-
-    Rect roi1, roi2;
-    Mat Q;
-
-    if( !intrinsic_filename.empty() )
-    {
-        // reading intrinsic parameters
-        FileStorage fs(intrinsic_filename, FileStorage::READ);
-        if(!fs.isOpened())
+        std::cout << "Video capturing has been started ..." << std::endl;
+//        capture.set(CV_CAP_PROP_FRAME_WIDTH,2560);    //  1280x720
+//        capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
+        capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);  //640x480
+        capture.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+        //capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('M','J','P','G'));
+        capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('Y','U','Y','V'));
+        //capture.set(CV_CAP_PROP_FOURCC,CV_FOURCC('Y','V','1','2'));
+        for(;;)
         {
-            printf("Failed to open file %s\n", intrinsic_filename.c_str());
-            return -1;
+            capture >> frame;
+            left_img = frame(Rect(0,0, frame.cols / 2, frame.rows));
+            right_img = frame(Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows));
+            if( frame.empty() )
+                break;
+
+//            Mat frame1 = frame.clone();
+//            imshow("camera", frame);
+//            imshow("left image", left_img);
+//            imshow("right image", right_img);
+
+            char c = (char)waitKey(10);
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+
+
+            Mat img1 = left_img;
+            Mat img2 = right_img;
+
+            if (img1.empty())
+            {
+                printf("Command-line parameter error: could not load the first input image file\n");
+                return -1;
+            }
+            if (img2.empty())
+            {
+                printf("Command-line parameter error: could not load the second input image file\n");
+                return -1;
+            }
+
+            if (scale != 1.f)
+            {
+                Mat temp1, temp2;
+                int method = scale < 1 ? INTER_AREA : INTER_CUBIC;
+                resize(img1, temp1, Size(), scale, scale, method);
+                img1 = temp1;
+                resize(img2, temp2, Size(), scale, scale, method);
+                img2 = temp2;
+            }
+
+            Size img_size = img1.size();
+
+            Rect roi1, roi2;
+            Mat Q;
+
+            if( !intrinsic_filename.empty() )
+            {
+                // reading intrinsic parameters
+                FileStorage fs(intrinsic_filename, FileStorage::READ);
+                if(!fs.isOpened())
+                {
+                    printf("Failed to open file %s\n", intrinsic_filename.c_str());
+                    return -1;
+                }
+
+                Mat M1, D1, M2, D2;
+                fs["M1"] >> M1;
+                fs["D1"] >> D1;
+                fs["M2"] >> M2;
+                fs["D2"] >> D2;
+
+                M1 *= scale;
+                M2 *= scale;
+
+                fs.open(extrinsic_filename, FileStorage::READ);
+                if(!fs.isOpened())
+                {
+                    printf("Failed to open file %s\n", extrinsic_filename.c_str());
+                    return -1;
+                }
+
+                Mat R, T, R1, P1, R2, P2;
+                fs["R"] >> R;
+                fs["T"] >> T;
+
+                stereoRectify( M1, D1, M2, D2, img_size, R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY, -1, img_size, &roi1, &roi2 );
+
+                Mat map11, map12, map21, map22;
+                initUndistortRectifyMap(M1, D1, R1, P1, img_size, CV_16SC2, map11, map12);
+                initUndistortRectifyMap(M2, D2, R2, P2, img_size, CV_16SC2, map21, map22);
+
+                Mat img1r, img2r;
+                remap(img1, img1r, map11, map12, INTER_LINEAR);
+                remap(img2, img2r, map21, map22, INTER_LINEAR);
+
+                img1 = img1r;
+                img2 = img2r;
+            }
+
+            numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : ((img_size.width/8) + 15) & -16;
+
+            bm->setROI1(roi1);
+            bm->setROI2(roi2);
+            bm->setPreFilterCap(31);
+            bm->setBlockSize(SADWindowSize > 0 ? SADWindowSize : 9);
+            bm->setMinDisparity(0);
+            bm->setNumDisparities(numberOfDisparities);
+            bm->setTextureThreshold(10);
+            bm->setUniquenessRatio(15);
+            bm->setSpeckleWindowSize(100);
+            bm->setSpeckleRange(32);
+            bm->setDisp12MaxDiff(1);
+
+            sgbm->setPreFilterCap(63);
+            int sgbmWinSize = SADWindowSize > 0 ? SADWindowSize : 3;
+            sgbm->setBlockSize(sgbmWinSize);
+
+            int cn = img1.channels();
+
+            sgbm->setP1(8*cn*sgbmWinSize*sgbmWinSize);
+            sgbm->setP2(32*cn*sgbmWinSize*sgbmWinSize);
+            sgbm->setMinDisparity(0);
+            sgbm->setNumDisparities(numberOfDisparities);
+            sgbm->setUniquenessRatio(10);
+            sgbm->setSpeckleWindowSize(100);
+            sgbm->setSpeckleRange(32);
+            sgbm->setDisp12MaxDiff(1);
+            if(alg==STEREO_HH)
+                sgbm->setMode(StereoSGBM::MODE_HH);
+            else if(alg==STEREO_SGBM)
+                sgbm->setMode(StereoSGBM::MODE_SGBM);
+            else if(alg==STEREO_3WAY)
+                sgbm->setMode(StereoSGBM::MODE_SGBM_3WAY);
+
+            Mat disp, disp8;
+            //Mat img1p, img2p, dispp;
+            //copyMakeBorder(img1, img1p, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
+            //copyMakeBorder(img2, img2p, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
+
+            int64 t = getTickCount();
+            if( alg == STEREO_BM )
+                bm->compute(img1, img2, disp);
+            else if( alg == STEREO_SGBM || alg == STEREO_HH || alg == STEREO_3WAY )
+                sgbm->compute(img1, img2, disp);
+            t = getTickCount() - t;
+            printf("Time elapsed: %fms\n", t*1000/getTickFrequency());
+
+            //disp = dispp.colRange(numberOfDisparities, img1p.cols);
+            if( alg != STEREO_VAR )
+                disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities*16.));
+            else
+                disp.convertTo(disp8, CV_8U);
+            if( !no_display )
+            {
+                namedWindow("left", 1);
+                imshow("left", img1);
+                namedWindow("right", 1);
+                imshow("right", img2);
+                namedWindow("disparity", 1);
+                imshow("disparity", disp8);
+                //printf("disp8 dimension: %d, %d", disp8.rows, disp8.cols);
+                fflush(stdout);
+                //waitKey();
+                printf("\n");
+            }
+
+            if(!disparity_filename.empty())
+                imwrite(disparity_filename, disp8);
+
+            if(!point_cloud_filename.empty())
+            {
+                printf("storing the point cloud...");
+                fflush(stdout);
+                Mat xyz;
+                reprojectImageTo3D(disp, xyz, Q, true);
+                saveXYZ(point_cloud_filename.c_str(), xyz);
+                printf("\n");
+            }
+
+
         }
-
-        Mat M1, D1, M2, D2;
-        fs["M1"] >> M1;
-        fs["D1"] >> D1;
-        fs["M2"] >> M2;
-        fs["D2"] >> D2;
-
-        M1 *= scale;
-        M2 *= scale;
-
-        fs.open(extrinsic_filename, FileStorage::READ);
-        if(!fs.isOpened())
-        {
-            printf("Failed to open file %s\n", extrinsic_filename.c_str());
-            return -1;
-        }
-
-        Mat R, T, R1, P1, R2, P2;
-        fs["R"] >> R;
-        fs["T"] >> T;
-
-        stereoRectify( M1, D1, M2, D2, img_size, R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY, -1, img_size, &roi1, &roi2 );
-
-        Mat map11, map12, map21, map22;
-        initUndistortRectifyMap(M1, D1, R1, P1, img_size, CV_16SC2, map11, map12);
-        initUndistortRectifyMap(M2, D2, R2, P2, img_size, CV_16SC2, map21, map22);
-
-        Mat img1r, img2r;
-        remap(img1, img1r, map11, map12, INTER_LINEAR);
-        remap(img2, img2r, map21, map22, INTER_LINEAR);
-
-        img1 = img1r;
-        img2 = img2r;
-    }
-
-    numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : ((img_size.width/8) + 15) & -16;
-
-    bm->setROI1(roi1);
-    bm->setROI2(roi2);
-    bm->setPreFilterCap(31);
-    bm->setBlockSize(SADWindowSize > 0 ? SADWindowSize : 9);
-    bm->setMinDisparity(0);
-    bm->setNumDisparities(numberOfDisparities);
-    bm->setTextureThreshold(10);
-    bm->setUniquenessRatio(15);
-    bm->setSpeckleWindowSize(100);
-    bm->setSpeckleRange(32);
-    bm->setDisp12MaxDiff(1);
-
-    sgbm->setPreFilterCap(63);
-    int sgbmWinSize = SADWindowSize > 0 ? SADWindowSize : 3;
-    sgbm->setBlockSize(sgbmWinSize);
-
-    int cn = img1.channels();
-
-    sgbm->setP1(8*cn*sgbmWinSize*sgbmWinSize);
-    sgbm->setP2(32*cn*sgbmWinSize*sgbmWinSize);
-    sgbm->setMinDisparity(0);
-    sgbm->setNumDisparities(numberOfDisparities);
-    sgbm->setUniquenessRatio(10);
-    sgbm->setSpeckleWindowSize(100);
-    sgbm->setSpeckleRange(32);
-    sgbm->setDisp12MaxDiff(1);
-    if(alg==STEREO_HH)
-        sgbm->setMode(StereoSGBM::MODE_HH);
-    else if(alg==STEREO_SGBM)
-        sgbm->setMode(StereoSGBM::MODE_SGBM);
-    else if(alg==STEREO_3WAY)
-        sgbm->setMode(StereoSGBM::MODE_SGBM_3WAY);
-
-    Mat disp, disp8;
-    //Mat img1p, img2p, dispp;
-    //copyMakeBorder(img1, img1p, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
-    //copyMakeBorder(img2, img2p, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
-
-    int64 t = getTickCount();
-    if( alg == STEREO_BM )
-        bm->compute(img1, img2, disp);
-    else if( alg == STEREO_SGBM || alg == STEREO_HH || alg == STEREO_3WAY )
-        sgbm->compute(img1, img2, disp);
-    t = getTickCount() - t;
-    printf("Time elapsed: %fms\n", t*1000/getTickFrequency());
-
-    //disp = dispp.colRange(numberOfDisparities, img1p.cols);
-    if( alg != STEREO_VAR )
-        disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities*16.));
-    else
-        disp.convertTo(disp8, CV_8U);
-    if( !no_display )
-    {
-        namedWindow("left", 1);
-        imshow("left", img1);
-        namedWindow("right", 1);
-        imshow("right", img2);
-        namedWindow("disparity", 0);
-        imshow("disparity", disp8);
-        printf("press any key to continue...");
-        fflush(stdout);
-        waitKey();
-        printf("\n");
-    }
-
-    if(!disparity_filename.empty())
-        imwrite(disparity_filename, disp8);
-
-    if(!point_cloud_filename.empty())
-    {
-        printf("storing the point cloud...");
-        fflush(stdout);
-        Mat xyz;
-        reprojectImageTo3D(disp, xyz, Q, true);
-        saveXYZ(point_cloud_filename.c_str(), xyz);
-        printf("\n");
     }
 
     return 0;
+
+
+
 }
---
+++ test.sh
@@ -0,0 +1,1 @@
+./stereo_match --blocksize=5 --max-disparity=64 -i=../intrinsics.yml -e=../extrinsics.yml --scale=1.0 --algorithm=sgbm
