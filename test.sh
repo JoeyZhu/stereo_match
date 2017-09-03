774cacb0cecdb344d74bc444acc677458c5a1b97
joey <joey@joey-nb>
2017-09-03 13:35:50 +0800

    # WARNING: head commit changed in the meantime
    init commit

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-./stereo_match --blocksize=11 --max-disparity=96 -i=intrinsics.yml -e=extrinsics.yml --scale=0.5 --algorithm=sgbm --device_no=1
+build/stereo_match --blocksize=11 --max-disparity=96 -i=intrinsics.yml -e=extrinsics.yml --scale=0.5 --algorithm=sgbm --device_no=1
